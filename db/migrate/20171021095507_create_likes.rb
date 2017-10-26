d5c0ded73abc3e143ecb15a231fddcec3edb3c24
Hideki Yoshitomi <hideyoshi@MacBook-Pro.local>
2017-10-26 17:35:24 +0900

    db references

--- db/migrate/20171021095507_create_likes.rb
+++ db/migrate/20171021095507_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[5.1]
   def change
     create_table :likes do |t|
-      t.reference :user_id, foreign_key: true
-      t.reference :listing_id, foreign_key: true
+      t.references :user_id, foreign_key: true
+      t.references :listing_id, foreign_key: true
 
       t.timestamps
     end
