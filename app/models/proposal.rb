510c2194e9e168e480c27f2030043506ad21df41
Hideki Yoshitomi <hideyoshi@MacBook-Pro.local>
2017-11-01 15:42:30 +0900

    fix layout

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -4,4 +4,9 @@ class Proposal < ApplicationRecord
 
   has_many :approvals
 
+  validates :user_id, presence: true
+  validates :listing_id, presence: true
+  validates :proposal_content, presence: true
+  validates :proposal_price, presence: true
+
 end
